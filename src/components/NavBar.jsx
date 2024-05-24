81bb4ad0c28bb1b2666b6e23fd664f0e6c8a28f4
wasp <wasp123321@gmail.com>
2024-05-24 16:01:02 +0200

    Completed exercise 7.21

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,17 +16,33 @@ const NavBar = () => {
   }
 
   return (
-    <><div className='bg-info bg-gradient p-2 '>
-      <Link to='/' className='me-2 btn btn-primary' >Blogs</Link>
-      <Link to='/users' className='me-2 btn btn-primary' >Users</Link>
-      <span >
-        {user.username} logged in
-        <button className='btn btn-light ms-3' onClick={logoutUser}>logout</button>
-      </span>
-    </div>
-    <div>
-      <h2>Blog App</h2>
-    </div></>
+    <>
+      <nav className='navbar navbar-expand-sm'>
+        <div className='container row position-relative bg-info bg-gradient p-2 w-100'>
+          <div className='col'>
+            <button className='navbar-toggler' data-bs-toggle='collapse' data-bs-target='#nav'>
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div className="collapse navbar-collapse" id='nav'>
+              <ul className='navbar-nav'>
+                <li className='nav-item my-1 my-sm-0'><Link to='/' className='me-2 btn btn-primary' >Blogs</Link></li>
+                <li className='nav-item my-1 my-sm-0'><Link to='/users' className='me-2 btn btn-primary' >Users</Link></li>
+              </ul>
+            </div>
+          </div>
+          <div className='col'>
+            <span className='position-absolute top-0 end-0 m-2'>
+              {user.username} logged in
+              <button className='btn btn-light ms-3' onClick={logoutUser}>logout</button>
+            </span>
+          </div>
+
+        </div>
+      </nav>
+      <div>
+        <h2>Blog App</h2>
+      </div>
+    </>
   )
 }
 
